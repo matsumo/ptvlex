30280ff36053d2fcc105c0e4115084bd5d3df020
matsumo <matsumo@ce.ns0.it>
2015-10-20 11:38:13 +0900

    first version(support Japanese only)

---
+++ app/src/main/java/com/matsumo/ptvlex/Patch.java
@@ -0,0 +1,43 @@
+/**
+ * ptvlex Copyright (C) 2015 matsumo All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.matsumo.ptvlex;
+
+import java.util.AbstractMap;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class Patch implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        // probably works only
+        // versionCode="21", versionName="5.0.1-1624448"
+        // it is dexguarded.
+        if (loadPackageParam.packageName.equals("com.getpebble.android.basalt")) {
+            findAndHookMethod("com.getpebble.android.onboarding.fragment.ChooseLanguageFragment",
+                loadPackageParam.classLoader, "a", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        AbstractMap<String, String> list = (AbstractMap<String, String>) param.getResult();
+                        list.put("jpn-JPN", "ja_JP");       // Japanese
+                    }
+                });
+        }
+    }
+}
